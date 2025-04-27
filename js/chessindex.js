830b27a2c0d30074601223dc9d0a0c2b15a0d1d2
unknown <lucas.kawakami@sptech.school>
2025-04-27 20:57:22 -0300

    banner

--- js/chessindex.js
+++ js/chessindex.js
@@ -1,84 +1,34 @@
-
 var board = null
 var game = new Chess()
-var $status = $('#status')
-var $fen = $('#fen')
-var $pgn = $('#pgn')
+var onnn=false
 
-function onDragStart(source, piece, position, orientation) {
-    // do not pick up pieces if the game is over
-    if (game.game_over()) return false
-
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-        return false
-    }
-}
-
-function onDrop(source, target) {
-    // see if the move is legal
-    var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q' // NOTE: always promote to a queen for example simplicity
-    })
-
-    // illegal move
-    if (move === null) return 'snapback'
-
-    updateStatus()
-}
-
-// update the board position after the piece snap
-// for castling, en passant, pawn promotion
-function onSnapEnd() {
-
-    board.position(game.fen())
-}
-
-function updateStatus() {
-    var status = ''
-
-    var moveColor = 'White'
-    if (game.turn() === 'b') {
-        moveColor = 'Black'
-    }
-
-    // checkmate?
-    if (game.in_checkmate()) {
-        status = 'Game over, ' + moveColor + ' is in checkmate.'
-    }
-
-    // draw?
-    else if (game.in_draw()) {
-        status = 'Game over, drawn position'
-    }
-
-    // game still on
-    else {
-        status = moveColor + ' to move'
-
-        // check?
-        if (game.in_check()) {
-            status += ', ' + moveColor + ' is in check'
-        }
-    }
-
-    $status.html(status)
-    $fen.html(game.fen())
-    $pgn.html(game.pgn())
-}
-
-var config = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd
+function ligar() {
+    onnn =true
+    console.log('a') 
 
 }
-board = Chessboard('myBoard', config)
+function desligar( ) {
+    onnn =false
+}
 
-updateStatus()
+function makeRandomMove () {
+    if (onnn) { 
+        var possibleMoves = game.moves()
+        
+        // exit if the game is over
+        if (game.game_over()) return
+        
+        var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+        game.move(possibleMoves[randomIdx])
+        board.position(game.fen())
+         
+    }
+    window.setTimeout(makeRandomMove, 500);
+} 
 
+board = Chessboard('myBoard', 'start')
+
+$('#myBoard').on('mouseenter', ligar); // jQuery
+$('#myBoard').on('mouseleave', desligar);
+
+makeRandomMove();
