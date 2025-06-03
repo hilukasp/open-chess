686910571e78cce122c48b55f62ccacfa0fa38b2
hiluka <lucas.kawakami@sptech.school>
2025-06-03 14:02:34 -0300

    versao final

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idUsuario) {
-
+    //mostra todos os usuarios e o flashcards atrelado a ela
     var instrucaoSql = `select jogador.* , historico.* from user jogador 
     inner join posicao_flashcard historico on jogador.id=historico.id_user 
     where jogador.id=${idUsuario} order by revisao desc`;
@@ -11,15 +11,14 @@ function buscarUltimasMedidas(idUsuario) {
 }
 
 function buscarMedidasEmTempoReal(idUsuario) {
-
+    //mostra as respectivas aberturas e soma os corretos
     var instrucaoSql = `select 
-  abertura.nome_categoria,
-  sum(case when historico.correto = true then 1 else 0 end) as correto 
-from posicao_flashcard historico inner join posicao_chess abertura 
-on historico.id_posicao = abertura.id
-where historico.id_user = ${idUsuario}
-group by abertura.nome_categoria;
- `;
+    abertura.nome_categoria,
+    sum(case when historico.correto = true then 1 else 0 end) as correto 
+    from posicao_flashcard historico inner join posicao_chess abertura 
+    on historico.id_posicao = abertura.id
+    where historico.id_user = ${idUsuario}
+    group by abertura.nome_categoria;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
